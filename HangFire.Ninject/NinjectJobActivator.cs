1bc4ab849b27b163c203fdcd5b9409030f1c3a36
Sergey Odinokov <odinserj@gmail.com>
2014-04-01 16:41:41 +0400

    Some doc comments

--- HangFire.Ninject/NinjectJobActivator.cs
+++ HangFire.Ninject/NinjectJobActivator.cs
@@ -3,10 +3,19 @@ using Ninject;
 
 namespace HangFire
 {
+    /// <summary>
+    /// HangFire Job Activator based on Ninject IoC Container.
+    /// </summary>
     public class NinjectJobActivator : JobActivator
     {
         private readonly IKernel _kernel;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NinjectJobActivator"/>
+        /// class with a given Ninject Kernel.
+        /// </summary>
+        /// <param name="kernel">Kernel that will be used to create instance
+        /// of classes during job activation process.</param>
         public NinjectJobActivator(IKernel kernel)
         {
             if (kernel == null) throw new ArgumentNullException("kernel");
@@ -14,6 +23,7 @@ namespace HangFire
             _kernel = kernel;
         }
 
+        /// <inheritdoc />
         public override object ActivateJob(Type jobType)
         {
             return _kernel.Get(jobType);
