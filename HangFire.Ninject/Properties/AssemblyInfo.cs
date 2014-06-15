c7733b802862946279106570dcd784dc56531876
Sergey Odinokov <odinserj@gmail.com>
2014-06-15 20:40:58 +0400

    Bump version to 1.0.0-alpha1

--- HangFire.Ninject/Properties/AssemblyInfo.cs
+++ HangFire.Ninject/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("91f25a4e-65e7-4d9c-886e-33a6c82b14c4")]
 
-[assembly: AssemblyVersion("0.1.2")]
+[assembly: AssemblyVersion("1.0.0-alpha1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
