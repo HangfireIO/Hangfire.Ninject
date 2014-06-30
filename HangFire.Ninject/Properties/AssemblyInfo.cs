f5c2466df5a26529783983d6f8c609cd2d47cbcb
Sergey Odinokov <odinserj@gmail.com>
2014-06-30 12:31:24 +0400

    Fix package and assembly metadata

--- HangFire.Ninject/Properties/AssemblyInfo.cs
+++ HangFire.Ninject/Properties/AssemblyInfo.cs
@@ -4,9 +4,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HangFire.Ninject")]
-[assembly: AssemblyDescription("Ninject IoC Container support for HangFire (background job system for ASP.NET applications).")]
-[assembly: AssemblyProduct("HangFire")]
+[assembly: AssemblyTitle("Hangfire.Ninject")]
+[assembly: AssemblyDescription("Ninject IoC Container support for Hangfire (background job system for ASP.NET applications).")]
+[assembly: AssemblyProduct("Hangfire")]
 [assembly: AssemblyCopyright("Copyright © 2014 Sergey Odinokov")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -17,6 +17,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("91f25a4e-65e7-4d9c-886e-33a6c82b14c4")]
 
-[assembly: AssemblyInformationalVersion("1.0.0-alpha2")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
