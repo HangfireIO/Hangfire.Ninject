b4a30eba94429103b82ca3856d59f104ce097947
Sergey Odinokov <odinserj@hangfire.io>
2024-02-19 16:43:25 +0700

    Sign NuGet and assembly artifacts during the build process

--- psake-project.ps1
+++ psake-project.ps1
@@ -15,6 +15,12 @@ Task Collect -Depends Test -Description "Copy all artifacts to the build folder.
 Task Pack -Depends Collect -Description "Create NuGet packages and archive files." {
     $version = Get-PackageVersion
 
-    Create-Archive "Hangfire.Ninject-$version"
     Create-Package "Hangfire.Ninject" $version
+    Create-Archive "Hangfire.Ninject-$version"
+}
+
+Task Sign -Depends Pack -Description "Sign artifacts." {
+    $version = Get-PackageVersion
+
+    Sign-ArchiveContents "Hangfire.Ninject-$version" "hangfire"
 }
